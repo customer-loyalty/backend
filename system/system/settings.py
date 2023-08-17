d4d2f6d2b5b7d15b55213eb26d9ca7303e90f8f3
Anton <A60874022@yandex.ru>
2023-08-17 20:56:08 +0500

    system_model

--- system/system/settings.py
+++ system/system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'accountapp.apps.AccountappConfig',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- system/system/urls.py
+++ system/system/urls.py
@@ -16,10 +16,41 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DevJunction API",
+        default_version="v1",
+        description="DevJunction  API",
+        terms_of_service="<https://www.google.com/policies/terms/>",
+        contact=openapi.Contact(email="contact@devjunction.in"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.IsAdminUser], # You can add your own permissions
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     # JWT-эндпоинты, для управления JWT-токенами:
     path('auth/', include('djoser.urls.jwt')),
+     re_path(
+        r"^swagger(?P<format>\\.json|\\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^api/docs/swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^api/docs/redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ] 
