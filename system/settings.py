10c21b077dcd2723cce61a67bc1fab2c0b137e0f
CarloDiPalma <ping.06@mail.ru>
2023-09-18 15:53:55 +0300

    Update cors

--- system/settings.py
+++ system/settings.py
@@ -47,11 +47,13 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "theproject.ddns.net"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "theproject.ddns.net"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -184,8 +186,6 @@ DJOSER = {
          'user_create': 'accountapp.serializers.AccountSerializer'
     }
 }
-
-
 
 #Email Configuration
 EMAIL_HOST = 'smtp.mail.ru'
